f6b9514fb25aaef811be92686157ed1892fe66e1
mwon <melvin.won@dhl.com>
2021-03-27 11:58:49 +0800

    commit

--- vars/workflow.groovy
+++ vars/workflow.groovy
@@ -3,4 +3,6 @@
 def deploymentComplete(Map config) {
     String _id        = config.id ?: 'undefined'
     String _status    = config.status ?: 'undefined'
+    echo "--- ${_id}"
+    echo "--- ${_status}"
 }
